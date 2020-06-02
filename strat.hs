042cd5ab6e1753fc8a08063f0a55386dba78a60b
rektrex <23721797+rektrex@users.noreply.github.com>
2020-06-02 16:19:06 +0530

    use rparWith and rdeepseq for parPair

--- strat.hs
+++ strat.hs
@@ -1,14 +1,17 @@
 import Control.Parallel.Strategies hiding (parPair)
 
-main = print ((fib 36, fib 35) `using` parPair)
-
-parPair :: Strategy (a,b)
-parPair (a,b) = do
-  a' <- rpar a
-  b' <- rpar b
-  return (a', b')
+main = print ((fib 36, fib 35) `using` (parPair rdeepseq rdeepseq))
 
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
+
+evalPair :: Strategy a -> Strategy b -> Strategy (a,b)
+evalPair sa sb (a,b) = do
+  a' <- sa a
+  b' <- sb b
+  return (a',b')
+
+parPair :: Strategy a -> Strategy b -> Strategy (a,b)
+parPair sa sb = evalPair (rparWith sa) (rparWith sb)
